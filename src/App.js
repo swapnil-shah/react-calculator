8de5b65a70c6422dfcae86119709d2d3916c808c
swapnil-shah <swapnilshah.me@gmail.com>
2020-12-02 20:19:09 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Calculator extends Component {
+  state = {
+    firstNumber: 0,
+    secondNumber: 0,
+    answer: ""
+  };
+
+  handleAdd = () => {
+    this.setState({ answer: this.state.firstNumber + this.state.secondNumber });
+  };
+  handleSubtract = () => {
+    this.setState({
+      answer: this.state.firstNumber - this.state.secondNumber
+    });
+  };
+  handleMultiply = () => {
+    this.setState({
+      answer: this.state.firstNumber * this.state.secondNumber
+    });
+  };
+  handleDivision = () => {
+    this.setState({
+      answer: this.state.firstNumber / this.state.secondNumber
+    });
+  };
+
+  handleClick = (event) => {
+    if (event.currentTarget.id === "add") {
+    }
+    event.currentTarget.id === "add"
+      ? this.setState({
+        answer: this.state.firstNumber + this.state.secondNumber
+      })
+      : event.currentTarget.id === "subtract"
+        ? this.setState({
+          answer: this.state.firstNumber - this.state.secondNumber
+        })
+        : event.currentTarget.id === "multiply"
+          ? this.setState({
+            answer: this.state.firstNumber * this.state.secondNumber
+          })
+          : this.setState({
+            answer: this.state.firstNumber / this.state.secondNumber
+          });
+  };
+  handleFirstNum = (event) => {
+    this.setState({ firstNumber: parseInt(event.target.value, 0) });
+  };
+  handleSecondNum = (event) => {
+    this.setState({ secondNumber: parseInt(event.target.value, 0) });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <h1>Calculate with React!</h1>
+          <div className="add">
+            <input type="number" onChange={this.handleFirstNum} />
+            <span>+</span>
+            <input type="number" onChange={this.handleSecondNum} />
+            <span>=</span>
+            <span>{this.state.answer}</span>
+          </div>
+          <button onClick={this.handleClick} id="add">
+            Add
+          </button>
+          <button onClick={this.handleClick} id="subtract">
+            Subtract
+          </button>
+          <button onClick={this.handleClick} id="multiply">
+            Multiply
+          </button>
+          <button onClick={this.handleClick} id="division">
+            Division
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Calculator;
